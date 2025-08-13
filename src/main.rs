4ded8841246164924e398eeeb205cf4bea1bb76a
Learnedprawn <vineshparalkar@gmail.com>
2025-08-13 07:20:08 +0530

    added read and return

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 #![allow(unused_imports)]
-use std::{io::Write, net::TcpListener};
+use std::{io::{Read, Write}, net::TcpListener};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -13,7 +13,12 @@ fn main() {
         match stream {
             Ok(mut stream) => {
                 println!("accepted new connection");
+                let mut buf = [0; 512];
                 loop {
+                    let bytes_read = stream.read(&mut buf).unwrap();
+                    if bytes_read == 0 {
+                        return;
+                    }
                     stream.write_all(b"+PONG\r\n").unwrap();
                 }
             }
