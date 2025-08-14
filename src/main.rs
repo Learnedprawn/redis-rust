1b78cc16e5cbaf2fe2af6016039d5c291b934b44
Learnedprawn <vineshparalkar@gmail.com>
2025-08-15 00:04:14 +0530

    message parsed

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,6 @@ const CR: u8 = b'\r';
 const LD: u8 = b'\n';
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
     // Uncomment this block to pass the first stage
