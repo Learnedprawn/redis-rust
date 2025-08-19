3663e7517fd14a1f2edb11dda6ba6c77ba8b110a
Learnedprawn <vineshparalkar@gmail.com>
2025-08-19 20:09:33 +0530

    formatting

--- src/main.rs
+++ src/main.rs
@@ -42,8 +42,6 @@ enum RedisBufSplit {
 fn main() {
     println!("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-    //
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
