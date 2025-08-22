d30e7168b970c5846f8d46189a151f639b130717
Learnedprawn <vineshparalkar@gmail.com>
2025-08-22 08:10:49 +0530

    inprogress keystore mutex arc

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,19 @@
 mod parser;
 use crate::parser::{encode, redis_parse, RESPError, RedisBufSplit};
 use std::{
+    collections::HashMap,
     io::{Read, Write},
     net::TcpListener,
+    sync::{Arc, Mutex},
     thread::spawn,
 };
-
-// use bytes::{Bytes, BytesMut};
 
 fn main() {
     println!("Logs from your program will appear here!");
 
+    let mut keystore: Arc<Mutex<HashMap<Vec<u8>, Vec<u8>>>> = Arc::new(Mutex::new(HashMap::new()));
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    let store1 = Arc::clone(&keystore);
 
     for stream in listener.incoming() {
         match stream {
@@ -50,11 +52,27 @@ fn main() {
                                             stream.write_all(output.as_bytes()).unwrap();
                                         }
                                         b"SET" => {
-                                            let key = &values[1];
+                                            let key = if let RedisBufSplit::String(key) = &values[1]
+                                            {
+                                                println!("Key: {:?}", key);
+                                                key
+                                            } else {
+                                                panic!("Key Issue")
+                                            };
                                             let value = &values[2];
+                                            let value =
+                                                if let RedisBufSplit::String(value) = &values[2] {
+                                                    println!("Value: {:?}", value);
+                                                    key
+                                                } else {
+                                                    panic!("Value Issue")
+                                                };
                                             println!("SET was called");
-                                            let output = encode(key, &buf);
-                                            println!("Output: {:?}", output);
+                                            let mut store1_unlocked = store1.lock().unwrap();
+                                            store1_unlocked.insert(
+                                                key.as_slice(&buf).to_vec(),
+                                                value.as_slice(&buf).to_vec(),
+                                            );
                                             stream.write_all(b"+OK\r\n").unwrap();
                                         }
                                         b"GET" => {
